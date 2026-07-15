d18bcebb01043510ad1e2f3a7fc65140a174c879
EvgeStuduNetol <cptisotas@mail.ru>
2026-07-15 23:09:48 +0700

    HW 2-1

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,14 @@
 
 int main()
 {
-	std::cout << "Evgeniy\n";
-	std::cout << 30 << "\n";
+	int numb;
+
+	std::cout << "Enter a number: " << std::endl;
+	std::cin >> numb;
+	std::cout << "\n";
+
+	std::cout << "Your number: " << std::endl;
+	std::cout << numb << std::endl;
+
+	return 0;
 }
