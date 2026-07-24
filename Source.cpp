d167d462162bdc56c9675270ae5d640c387ea0ef
EvgeStuduNetol <cptisotas@mail.ru>
2026-07-25 02:01:29 +0700

    HW 3-1

--- Source.cpp
+++ Source.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
 #include <string>
-
+#include <Windows.h>
+// На моем Visual studio так и не хочет писаться на русском языке
 int main()
 {
-	std::string word;
+	std::cout << std::boolalpha;
+	std::cout << "Operator: ||" << std::endl;
+	
+	bool a = true, b = true;	
+	std::cout << a << " " << b << " " << (a || b) << std::endl;
 
-	std::cout << "Enter a word: " << std::endl;
-	std::cin >> word;
+	a = false, b = true;
+	std::cout << a << " " << b << " " << (a || b) << std::endl;
+
+	a = true, b = false;
+	std::cout << a << " " << b << " " << (a || b) << std::endl;
+
+	a = false, b = false;
+	std::cout << a << " " << b << " " << (a || b) << std::endl;
 	std::cout << "\n";
 
-	std::cout << "Your word: " << std::endl;
-	std::cout << word << std::endl;
+	std::cout << "Operator: &&" << std::endl;
+
+	a = true, b = true;
+	std::cout << a << " " << b << " " << (a && b) << std::endl;
+
+	a = false, b = true;
+	std::cout << a << " " << b << " " << (a && b) << std::endl;
+
+	a = true, b = false;
+	std::cout << a << " " << b << " " << (a && b) << std::endl;
+
+	a = false, b = false;
+	std::cout << a << " " << b << " " << (a && b) << std::endl;
 
 	return 0;
 }
