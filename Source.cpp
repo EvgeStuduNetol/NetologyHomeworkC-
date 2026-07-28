a3bf06f6e8590ee865f21f59c8d9ef9118f5e476
EvgeStuduNetol <cptisotas@mail.ru>
2026-07-28 22:24:31 +0700

    HW 4-1

--- Source.cpp
+++ Source.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
-#include <string>
-#include <Windows.h>
-// На моем Visual studio так и не хочет писаться на русском языке
+#include <windows.h>
+
 int main()
 {
-	double a, b, c;
+	SetConsoleCP(65001);
+	SetConsoleOutputCP(65001);
 
-	std::cout << "Enter your first number: ";
-	std::cin >> a;
+	int number = 0;
+	int sum = 0;
 
-	std::cout << "Enter your second number: ";
-	std::cin >> b;
+	do
+	{
+		std::cout << "Введите целое число или число '0', чтобы закончить:" << std::endl;
+		std::cin >> number;
 
-	std::cout << "Enter your third number: ";
-	std::cin >> c;
-
+		sum = sum + number;
+	} 
 	
-	int max, mid, min;
-	//вот тут математика у меня вышла из чата. пришлось прибегать к помощи интернета
-	max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
-	min = (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
-	mid = a + b + c - max - min; 
+	while (number != 0);
 
-	std::cout << "Result: " << max << " " << mid << " " << min << std::endl;
-
+	std::cout << "Сумма: " << sum << std::endl;
 
 	return 0;
 }
