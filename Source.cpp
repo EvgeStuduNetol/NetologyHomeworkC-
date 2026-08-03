a2b8c5de485ce76df64939daa68b27e5a6c43056
EvgeStuduNetol <cptisotas@mail.ru>
2026-08-04 00:29:57 +0700

    HW5-4

--- Source.cpp
+++ Source.cpp
@@ -7,56 +7,42 @@ int main()
 	SetConsoleCP(65001);
 	SetConsoleOutputCP(65001);
 
+	int array[10];
 
-	int array[3][6];
-	int min = array[0][0];
-	int max = array[0][0];
-	int minStr = 0; //строка минимального элемента
-	int minStl = 0; //столбец минимального элемента
-	int maxStr = 0; //строка максимального элемента
-	int maxStl = 0; //столбец максимального элемента
-
-	for (int i = 0; i < 3;i++)
+	for (int i = 0; i < 10; i++)
 	{
-		for (int j=0;j<6;j++)
+		array[i] = rand() % 101;	
+	}
+
+	std::cout << "Массив до сортировки: ";
+	for (int i = 0; i < 10;i++)
+	{
+			std::cout << array[i] << " ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "Массив после сортировки: ";
+	for (int k = 0; k < 9; k++)
+	{
+		int swap_counter = 0;
+		for (int i = 0; i < 9 - k; i++)
 		{
-			array[i][j] = rand() % 101;
+			if (array[i] < array[i + 1])
+			{
+				std::swap(array[i], array[i + 1]);
+				swap_counter++;
+			}
+		}
+		if (swap_counter == 0) 
+		{
+			break;
 		}
 	}
 
-	std::cout << "Массив: " << std::endl;
-
-	for (int i = 0; i < 3;i++)
+	for (int i = 0; i < 10;i++)
 	{
-		for (int j = 0; j < 6;j++)
-		{
-			std::cout << array[i][j] << "\t";
-
-			if (array[i][j] < min)
-			{
-				min = array[i][j];
-				minStr = i;
-				minStl = j;
-				
-			}
-
-			if (array[i][j] > max)
-			{
-				max = array[i][j];
-				maxStr = i;
-				maxStl = j;
-			}
-
-			
-		}
-		std::cout << std::endl;
+		std::cout << array[i] << " ";
 	}
-
-
-
-	std::cout << "Индекс минимального элемента: " << minStr << ' ' << minStl << std::endl;
-	std::cout << "Индекс максимального элемента: " << maxStr <<' ' << maxStl << std::endl;
-
 	return 0;
 }
 
