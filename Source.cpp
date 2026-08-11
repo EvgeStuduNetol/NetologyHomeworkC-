24b9e3ccd0840f8de0615319af55dcd2ea0ee12d
EvgeStuduNetol <cptisotas@mail.ru>
2026-08-11 17:27:28 +0700

    HW 7-1

--- Source.cpp
+++ Source.cpp
@@ -2,37 +2,27 @@
 #include <windows.h>
 #include <cstdlib>
 
-int result(int a)
-{
-	if (a == 0)
-	{
-		return 0;
-	}
-
-	if (a == 1) 
-	{
-		return 1;
-	}
-	return result(a - 1) + result(a - 2);
-}
-
 int main()
 {
 	SetConsoleCP(65001);
 	SetConsoleOutputCP(65001);
 
-	int a;
+	short a;
+	int b;
+	long c;
+	long long d;
+	float e;
+	double f;
+	long double g;
+	bool h;
 
-	std::cout << "Введите число: ";
-	std::cin >> a;
-	std::cout << std::endl;
-
-	std::cout << "Числа Фибоначчи : ";
-	for (int i = 0;i < a; i++) 
-	{
-		std::cout << result(i) << " ";
-	}
-	std::cout << std::endl;
-
+	std::cout << "Short: " << &a << " " << sizeof(a) << std::endl;
+	std::cout << "Int: " << &b << " " << sizeof(b) << std::endl;
+	std::cout << "Long: " << &c << " " << sizeof(c) << std::endl;
+	std::cout << "Long long: " << &d << " " << sizeof(d) << std::endl;
+	std::cout << "Float: " << &e << " " << sizeof(e) << std::endl;
+	std::cout << "Double: " << &f << " " << sizeof(f) << std::endl;
+	std::cout << "Long double: " << &g << " " << sizeof(g) << std::endl;
+	std::cout << "Bool: " << &h << " " << sizeof(h) << std::endl;
 	return 0;
 }
